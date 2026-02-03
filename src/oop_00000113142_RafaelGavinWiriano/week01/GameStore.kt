60a9250f7c04c00c650c4951bad0ae8eb4ded9dd
gavin973 <rafaelgavinwiriano1@gmail.com>
2026-02-04 02:18:02 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000113142_RafaelGavinWiriano/week01/GameStore.kt
+++ src/oop_00000113142_RafaelGavinWiriano/week01/GameStore.kt
@@ -1,2 +1,10 @@
 package oop_00000113142_RafaelGavinWiriano.week01
 
+fun main() {
+    val gameTitle = "wukong"
+    val price = 1000000
+
+    println(calculateDiscount(price))
+}
+
+fun calculateDiscount(price: Int): Int = if (price > 500000) price - (price * 20/100) else price - (price * 10/100)
