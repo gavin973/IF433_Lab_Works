8ce045846df47ac55e7ee087005c72625803fa9c
gavin973 <rafaelgavinwiriano1@gmail.com>
2026-02-04 01:54:56 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_00000113142_RafaelGavinWiriano/week01/CircleCalculator.kt
+++ src/oop_00000113142_RafaelGavinWiriano/week01/CircleCalculator.kt
@@ -1,17 +1,13 @@
 package oop_00000113142_RafaelGavinWiriano.week01
 
-fun main(args: Array<String>) {
-    // Variable definition
+fun main() {
+
     val radius = 7.0
     val pi = 3.14
 
-    // Calculation
     val area = pi * radius * radius
 
-    // Output Concatenation
     println("Radius: $radius , Area: $area")
-
-
 
     println(checkSize(area))
 
