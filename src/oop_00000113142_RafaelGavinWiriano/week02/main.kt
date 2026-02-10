6a9bf26a1940a7185f242b8df721166a77aa807f
gavin973 <rafaelgavinwiriano1@gmail.com>
2026-02-10 16:16:34 +0700

    week02: implement selection logic for constructors

--- src/oop_00000113142_RafaelGavinWiriano/week02/main.kt
+++ src/oop_00000113142_RafaelGavinWiriano/week02/main.kt
@@ -27,4 +27,21 @@ fun main (){
         val s1 = Student(name, nim, major)
         println("Status: Pendaftaran Selesai.")
     }
+
+    print("pilih jalur (1. reguler, 2. umum)")
+    val type = scanner.nextInt()
+    scanner.nextLine()
+
+
+    if (type == 1){
+        print("Masukan jurusan: ")
+        val major = scanner.nextLine()
+        val s1 = Student(name, nim, major = major)
+        println("terdaftar di: ${s1.major} dengan gpa awal ${s1.gpa}")
+    } else if (type == 2){
+        val s2 = Student(name, nim)
+        println("terdaftar di ${s2.major} dengan gpa awal ${s2.gpa}")
+    }else{
+        println("pilihan tidak ada, pendaftaran batal")
+    }
 }
