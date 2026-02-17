b25f98cea13c7e22da45a6a5de462dfe175a6b3a
gavin973 <rafaelgavinwiriano1@gmail.com>
2026-02-17 21:00:11 +0700

    week02: (task 1) implement library input system

---
+++ src/oop_00000113142_RafaelGavinWiriano/week02/main_2.kt
@@ -0,0 +1,31 @@
+package oop_00000113142_RafaelGavinWiriano.week02
+
+import java.util.Scanner
+
+fun main() {
+
+    val scanner = Scanner(System.`in`)
+
+    print("Masukkan Judul Buku: ")
+    val bookTitle = scanner.nextLine()
+
+    print("Masukkan Nama Peminjam: ")
+    val borrower = scanner.nextLine()
+
+    print("Masukkan Lama Pinjam (hari): ")
+    var duration = scanner.nextInt()
+
+    if (duration < 0) {
+        println("Durasi tidak boleh minus. Diubah menjadi 1 hari.")
+        duration = 1
+    }
+
+    val loan = Loan(bookTitle, borrower, duration)
+
+    println("\n===== DETAIL PEMINJAMAN =====")
+    println("Judul Buku : ${loan.bookTitle}")
+    println("Peminjam   : ${loan.borrower}")
+    println("Durasi     : ${loan.loanDuration} hari")
+    println("Total Denda: Rp ${loan.calculateFine()}")
+
+}
