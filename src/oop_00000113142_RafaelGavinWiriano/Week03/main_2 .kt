2267b411c179630f903461cb2ebcd08d3370703c
gavin973 <rafaelgavinwiriano1@gmail.com>
2026-02-23 23:38:12 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_00000113142_RafaelGavinWiriano/Week03/main_2 .kt
+++ src/oop_00000113142_RafaelGavinWiriano/Week03/main_2 .kt
@@ -1,2 +1,17 @@
 package oop_00000113142_RafaelGavinWiriano.Week03
 
+fun main() {
+
+    val weapon = Weapon("EMP Slayer", 300)
+
+    println("Test Damage -50")
+    weapon.damage = -50   // harus gagal
+
+    println("Damage yang sekarang: ${weapon.damage}")
+
+    println("\n Test Damage 9999 ")
+    weapon.damage = 9999  // harus jadi 1000
+
+    println("Damage yang sekarang: ${weapon.damage}")
+    println("Tiernya : ${weapon.tier}")
+}
--- src/oop_00000113142_RafaelGavinWiriano/Week03/weapon.kt
+++ src/oop_00000113142_RafaelGavinWiriano/Week03/weapon.kt
@@ -1,2 +1,29 @@
 package oop_00000113142_RafaelGavinWiriano.Week03
 
+class Weapon(
+    val name: String,
+    damage: Int
+) {
+
+    var damage: Int = damage
+        set(value) {
+            if (value < 0) {
+                println("Damage tidak boleh negatif!")
+                return
+            }
+
+            if (value > 1000) {
+                println("Damage terlalu besar! Maksimal 1000.")
+                field = 1000
+            } else {
+                field = value
+            }
+        }
+
+    val tier: String
+        get() = when {
+            damage > 800 -> "Legendary"
+            damage > 500 -> "Epic"
+            else -> "Common"
+        }
+}
