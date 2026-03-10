284250606fe86e463c63e2bf371491f3e610806a
gavin973 <rafaelgavinwiriano1@gmail.com>
2026-03-10 15:15:52 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000113142_RafaelGavinWiriano/Week06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_00000113142_RafaelGavinWiriano.Week06
+
+interface Camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan.") }
+}
