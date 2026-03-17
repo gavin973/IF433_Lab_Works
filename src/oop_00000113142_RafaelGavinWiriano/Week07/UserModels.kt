5367fe0f1f09edb76e02d0f095414cd71477de1e
gavin973 <rafaelgavinwiriano1@gmail.com>
2026-03-17 19:51:29 +0700

    week07: implement data class for structural equality

--- src/oop_00000113142_RafaelGavinWiriano/Week07/UserModels.kt
+++ src/oop_00000113142_RafaelGavinWiriano/Week07/UserModels.kt
@@ -1,4 +1,5 @@
 package oop_00000113142_RafaelGavinWiriano.Week07
 
-class RegularUser(val name: String, val age: Int) {
+class RegularUser(val name: String, val age: Int)
+data class DataUser(val name: String, val age: Int){
 }
