5f95ca79e4dac96515398e266e9de67c52da8cb2
gavin973 <rafaelgavinwiriano1@gmail.com>
2026-04-14 15:47:32 +0700

    week08: (task) create sealed class Product and data classes

---
+++ src/oop_00000113142_RafaelGavinWiriano/Week08/EcommerceModels.kt
@@ -0,0 +1,15 @@
+package oop_00000113142_RafaelGavinWiriano.Week08
+
+sealed class Product
+
+data class Electronic(
+    val id: String,
+    val name: String,
+    val warrantyMonths: Int
+) : Product()
+
+data class Clothing(
+    val id: String,
+    val name: String,
+    val size: String
+) : Product()
