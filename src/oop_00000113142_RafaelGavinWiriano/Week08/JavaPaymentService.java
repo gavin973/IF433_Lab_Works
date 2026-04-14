f066fd1e505e5792774676d078e7e5bd8a6735cf
gavin973 <rafaelgavinwiriano1@gmail.com>
2026-04-14 15:50:20 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000113142_RafaelGavinWiriano/Week08/JavaPaymentService.java
@@ -0,0 +1,8 @@
+package oop_00000113142_RafaelGavinWiriano.Week08;
+
+public class JavaPaymentService {
+
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
