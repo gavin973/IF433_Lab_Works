aa039ee65328ef87b8090e6b0088f9a354164a85
gavin973 <rafaelgavinwiriano1@gmail.com>
2026-05-06 23:22:41 +0700

    week10: (task) initialize and populate transaction repository

--- src/oop_00000113142_RafaelGavinWiriano/Week10/GenericBox/CryptoDashboard.kt
+++ src/oop_00000113142_RafaelGavinWiriano/Week10/GenericBox/CryptoDashboard.kt
@@ -2,10 +2,14 @@ package oop_00000113142_RafaelGavinWiriano.Week10.GenericBox
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+    val txRepo = WalletRepository<Transaction>()
 
     coinRepo.add(Coin("BTC", 1.5))
     coinRepo.add(Coin("ETH", 10.0))
     coinRepo.add(Coin("USDT", 1000.0))
+
+    txRepo.add(Transaction("TX001", 100.0))
+    txRepo.add(Transaction("TX002", 250.0))
 
     val response = ApiResponse("200 OK", coinRepo.getAll())
 
