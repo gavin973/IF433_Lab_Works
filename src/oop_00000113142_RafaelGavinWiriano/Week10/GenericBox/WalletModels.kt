f55f2e2b478f9cea49ccbfb80c55cd98779d4d2b
gavin973 <rafaelgavinwiriano1@gmail.com>
2026-05-06 23:14:17 +0700

    week10: (task) create Coin and Transaction models

---
+++ src/oop_00000113142_RafaelGavinWiriano/Week10/GenericBox/WalletModels.kt
@@ -0,0 +1,11 @@
+package oop_00000113142_RafaelGavinWiriano.Week10.GenericBox
+
+data class Coin(
+    val name: String,
+    val balance: Double
+)
+
+data class Transaction(
+    val id: String,
+    val amount: Double
+)
