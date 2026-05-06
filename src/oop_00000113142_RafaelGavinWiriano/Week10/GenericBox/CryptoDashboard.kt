ebf6f532b6166fa0f8443355d5bd2a0fedc02069
gavin973 <rafaelgavinwiriano1@gmail.com>
2026-05-06 23:20:38 +0700

    week10: (task) populate coin repository

--- src/oop_00000113142_RafaelGavinWiriano/Week10/GenericBox/CryptoDashboard.kt
+++ src/oop_00000113142_RafaelGavinWiriano/Week10/GenericBox/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_00000113142_RafaelGavinWiriano.Week10.GenericBox
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 1.5))
+    coinRepo.add(Coin("ETH", 10.0))
+    coinRepo.add(Coin("USDT", 1000.0))
 }
