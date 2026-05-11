9ef6137401856c0d7ae00d3fdea1d31ee3469221
gavin973 <rafaelgavinwiriano1@gmail.com>
2026-05-11 16:26:52 +0700

    week11: create nullable receiver extension

--- src/oop_00000113142_RafaelGavinWiriano/Wekk11/StringExtensions.kt
+++ src/oop_00000113142_RafaelGavinWiriano/Wekk11/StringExtensions.kt
@@ -1,2 +1,14 @@
 package oop_00000113142_RafaelGavinWiriano.Wekk11
 
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
+
+fun String?.isNullOrEmptyCustom(): Boolean {
+    return this == null || this.isEmpty()
+}
+
