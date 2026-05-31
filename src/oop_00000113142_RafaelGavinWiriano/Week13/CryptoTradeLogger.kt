9e14444eff1960540414a74973b0e7b373b1ee8d
gavin973 <rafaelgavinwiriano1@gmail.com>
2026-05-31 13:58:57 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_00000113142_RafaelGavinWiriano/Week13/CryptoTradeLogger.kt
@@ -0,0 +1,12 @@
+package oop_00000113142_RafaelGavinWiriano.Week13
+
+import java.io.File
+import java.io.FileNotFoundException
+
+data class TradeRecord(
+    val id: Int,
+    val symbol: String,
+    val type: String,
+    val margin: Double,
+    val pnl: Double
+)
