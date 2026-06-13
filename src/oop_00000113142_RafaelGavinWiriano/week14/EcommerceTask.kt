da709535e2091859c1cd449705407a206bff3438
gavin973 <rafaelgavinwiriano1@gmail.com>
2026-06-14 01:10:59 +0700

    week14: (task) implement PricingStrategy interface for OCP

--- src/oop_00000113142_RafaelGavinWiriano/week14/EcommerceTask.kt
+++ src/oop_00000113142_RafaelGavinWiriano/week14/EcommerceTask.kt
@@ -5,45 +5,41 @@ import java.io.File
 interface OrderRepository {
     fun save(orderData: String)
 }
-
 class CsvOrderRepository : OrderRepository {
-
     private val file = File("orders.csv")
-
     override fun save(orderData: String) {
         file.appendText("$orderData\n")
     }
 }
-
 interface NotificationService {
     fun send(message: String)
 }
-
 class EmailNotifier : NotificationService {
-
     override fun send(message: String) {
         println("Email terkirim: $message")
     }
 }
-
+interface PricingStrategy {
+    fun calculate(price: Double): Double
+}
+class VipPricing : PricingStrategy {
+    override fun calculate(price: Double) = price * 0.80
+}
+class RegularPricing : PricingStrategy {
+    override fun calculate(price: Double) = price
+}
 class SafeOrderProcessor(
     private val repo: OrderRepository,
-    private val notifier: NotificationService
+    private val notifier: NotificationService,
+    private val pricing: PricingStrategy
 ) {
-
     fun processOrder(
-        customerType: String,
         basePrice: Double,
         customerTypeName: String
     ) {
-
-        val finalPrice = when (customerType) {
-            "VIP" -> basePrice * 0.80
-            "REGULAR" -> basePrice
-            else -> basePrice
-        }
+        val finalPrice = pricing.calculate(basePrice)
         println("Memproses pesanan $customerTypeName seharga $finalPrice")
-        repo.save("$customerTypeName,$finalPrice,$customerType")
+        repo.save("$customerTypeName,$finalPrice")
         notifier.send("Pesanan $customerTypeName telah dikonfirmasi!")
     }
 }
